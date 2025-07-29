afbd886eb1af0729db4a28f3fceb30005ef5600d
iamzainmohsin <zainmohsinisgreat@gmail.com>
2025-07-29 10:08:28 +0000

    Loaded GUI

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow
+import os
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QVBoxLayout
 from ui.ui_image_viewer import Ui_MainWindow
 
-class Main_UI(QMainWindow):
+class MainUI(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-
-        self.ui.stacked_main.setCurrentIndex(0)
-        self.ui.importFld_btn.clicked.connect(self.handle_import)
-
-
-    def handle_import(self):
-        print("Import Folder button clicked")    
-
-
-
+       
+  
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Main_UI()
+    window = MainUI()
     window.show()
     sys.exit(app.exec())
+
