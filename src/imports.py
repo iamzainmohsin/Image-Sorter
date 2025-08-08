731c8a101d2b39089908238cd79b3659e2e9a360
iamzainmohsin <zainmohsinisgreat@gmail.com>
2025-08-08 11:28:08 +0000

    Added new Imports

--- src/imports.py
+++ src/imports.py
@@ -2,7 +2,7 @@
 # PyQt6 Core and GUI Elements
 from PyQt6.QtWidgets import QFileDialog, QListWidgetItem, QListWidget
 from PyQt6.QtGui import QPixmap, QIcon, QImage
-from PyQt6.QtCore import Qt, QSize, QTimer, QByteArray
+from PyQt6.QtCore import Qt, QSize, QTimer, QByteArray, QObject, QRunnable, QThreadPool, pyqtSignal, pyqtSlot, Qt
 
 # Standard Library
 import cv2
@@ -16,3 +16,4 @@ from image_sorter.backend.ImagesLogicManager import ImagesManager
 from image_sorter.gui.PYQT_Ui_File.ui_image_viewer import Ui_MainWindow
 from image_sorter.cpp import image_utils
 from image_sorter.cpp import thumbnail_utils
+from image_sorter.gui.ThumbnailWorker import ThumbnailTask
