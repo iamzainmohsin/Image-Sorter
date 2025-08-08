21ab358bb06908570fac11d2c0e1f9d172de0f95
iamzainmohsin <zainmohsinisgreat@gmail.com>
2025-08-08 17:00:48 +0000

    Ui Styling added

--- src/main.py
+++ src/main.py
@@ -4,6 +4,14 @@ from image_sorter.gui.GuiHandler import GuiHandler
 
 def main():
     app = QApplication(sys.argv)
+
+    try:
+        with open("src/image_sorter/gui/PYQT_Ui_File/style.qss", "r") as style_file:
+            style_sheet = style_file.read()
+            app.setStyleSheet(style_sheet)
+    except FileNotFoundError:
+        print("Style sheet file not found.")
+
     main_window = QMainWindow()
     gui = GuiHandler(main_window)
     main_window.show()
